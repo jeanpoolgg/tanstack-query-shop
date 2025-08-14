2a0f4c82e4169f8a1fa6dab46101234f6fe84fc4
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-14 17:46:01 -0500

    feat(products): filter by women's clothing category in women's page

--- src/products/pages/WomensPage.tsx
+++ src/products/pages/WomensPage.tsx
@@ -1,11 +1,20 @@
-import { ProductList } from ".."
+import { ProductList } from "../components/ProductList";
+import { useProducts } from "../hooks/useProducts";
 
 export const WomensPage = () => {
+
+  const { productsQuery } = useProducts({filterKey: "women's clothing"});
+
+  const products = productsQuery.data || [];
+
+
   return (
     <div className="flex-col">
       <h1 className="text-2xl font-bold">Productos para mujeres</h1>
 
-      <ProductList />
+      {productsQuery.isLoading && <p>Cargando...</p>}
+
+      <ProductList products={products} />
 
     </div>
   )
